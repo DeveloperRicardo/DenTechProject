d13059092e33fc4f2120e5521b06b3f272985676
Ricardo Miranda <rcrdo60@gmail.com>
2019-08-08 09:47:51 -0700

    Se creó la ventana de Inventario

---
+++ DenTech/WIN_CAT_ArchivosAdjuntos_T.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DenTech
+{
+    public partial class WIN_CAT_ArchivosAdjuntos_T : WIN_Template
+    {
+        public WIN_CAT_ArchivosAdjuntos_T()
+        {
+            InitializeComponent();
+        }
+    }
+}
