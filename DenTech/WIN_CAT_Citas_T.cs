2c2169cfc172255517943c92e14e1609b2e9f5a7
Ricardo Miranda <rcrdo60@gmail.com>
2019-08-19 08:13:21 -0700

    Se terminó el Calendario por mes, SERÁ MODIFICADO EL PRÓXIMO CUATRI...

--- DenTech/WIN_CAT_Citas_T.cs
+++ DenTech/WIN_CAT_Citas_T.cs
@@ -17,6 +17,7 @@ namespace DenTech
         ConexionSQL BD = new ConexionSQL();
         List<FlowLayoutPanel> Lista = new List<FlowLayoutPanel>();
         DateTime FechaActual = DateTime.Today;
+        int Id = 0;
         string FechaInicio = "";
 
         public WIN_CAT_Citas_T()
@@ -34,7 +35,6 @@ namespace DenTech
                 GenerarPaneles(42);
                 AgregarNumeroDia(ObtenerPrimerDiaMes(), ObtenerDiasMes());
                 CargarCitas(ObtenerPrimerDiaMes());
-                Refrescar();
 
                 // Muestra la información del mes y año actual
                 STC_MesAño.Text = this.FechaActual.ToString("MMMM, yyyy");
@@ -65,96 +65,10 @@ namespace DenTech
             MostrarFechaActual();
         }
 
-        // Evento del botón Agregar
-        private void BTN_Agregar_Click(object sender, EventArgs e)
-        {
-            // Se insatncia un objeto de tipo ventana para abrirla y refrescar la tabla
-            WIN_CAT_Citas_F Window = new WIN_CAT_Citas_F();
-            Window.ShowDialog();
-            Refrescar();
-        }
-
-        // Evento del botón Modificar
-        private void BTN_Modificar_Click(object sender, EventArgs e)
-        {
-            // Verifica que la tabla tenga información
-            if (DGV_TablaCitas.RowCount == 0)
-                return;
-
-            // Se insatncia un objeto de tipo ventana para abrirla y refrescar la tabla
-            WIN_CAT_Citas_F Window = new WIN_CAT_Citas_F((int)DGV_TablaCitas.CurrentRow.Cells[0].Value);
-            Window.ShowDialog();
-            Refrescar();
-        }
-
-        // Evento del botón Eliminar
-        private void BTN_Eliminar_Click(object sender, EventArgs e)
-        {
-            // Verifica que la tabla tenga información
-            if (DGV_TablaCitas.RowCount == 0)
-                return;
-
-            // Pregunta al usuario si desea eliminar el registro
-            if (MessageBox.Show("¿Desea eliminar el registro seleccionado?", "DenTech", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
-                // Se estructura el query para eliminar el registro
-                SqlCommand cmd = BD.conexion.CreateCommand();
-                cmd.CommandText = "Delete From CITAS Where Id_Cita = " + (int)DGV_TablaCitas.CurrentRow.Cells[0].Value;
-                cmd.ExecuteNonQuery(); // Se ejecuta
-
-                // Se confirma la eliminación del registro y se actualiza la información de la tabla
-                MessageBox.Show("Registro eliminado con éxito.", "DenTech", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                Refrescar();
-            }
-        }
-
         // Evento del botón Cancelar
         private void Button1_Click(object sender, EventArgs e)
         {
             this.Close(); // Se cierra la ventana
-        }
-
-        // Método Refrescar
-        public void Refrescar()
-        {
-            // Se realiza la conexión a la BD y se instancia un objeto de la misma, además se declaran variables
-            BD.conexion.CreateCommand();
-            SqlCommand cmd = BD.conexion.CreateCommand();
-            SqlDataAdapter Adaptador = new SqlDataAdapter();
-            var Data = new DataTable();
-
-            // Se estructura el query
-            cmd.CommandText = "Select " +
-                "Id_Cita,  " +
-                "CITAS.Id_Paciente,  " +
-                "NombreOdontologo = (EMPLEADOS.Nombre + ' ' + EMPLEADOS.ApellidoP + ' ' + EMPLEADOS.ApellidoM), " +
-                "NombrePaciente = (PACIENTES.Nombre + ' ' + PACIENTES.ApellidoP + ' ' + PACIENTES.ApellidoM), " +
-                "Fecha_Cita " +
-                "From CITAS " +
-                "Left Join EMPLEADOS on EMPLEADOS.Id_Empleado = CITAS.Id_Empleado " +
-                "Left Join PACIENTES on PACIENTES.Id_Paciente = CITAS.Id_Paciente " +
-                "Order By Fecha_Cita Desc";
-            cmd.ExecuteNonQuery(); // Se ejecuta
-
-            // Se crea un adaptador de sql, guardará el data source que contiene la información de la consulta
-            Adaptador.SelectCommand = cmd;
-            Adaptador.Fill(Data);
-
-            // Se inserta la información en el DataGridView
-            DGV_TablaCitas.DataSource = Data;
-        }
-
-        // Evento del botón Recetas
-        private void BTN_Recetas_Click(object sender, EventArgs e)
-        {
-            // Verifica que la tabla tenga información
-            if (DGV_TablaCitas.RowCount == 0)
-                return;
-
-            // Se insatncia un objeto de tipo ventana para abrirla y refrescar la tabla
-            WIN_CAT_Recetas_T Window = new WIN_CAT_Recetas_T((int)DGV_TablaCitas.CurrentRow.Cells[1].Value);
-            Window.ShowDialog();
-            Refrescar();
         }
 
         // Método GenerarCuadros
@@ -305,7 +219,7 @@ namespace DenTech
             }
         }
 
-        // Método AgregarCita
+        // Evento AgregarCita
         private void AgregarCita(object sender, EventArgs e)
         {
             // Se insatncia un objeto de tipo ventana para abrirla y refrescar la tabla
@@ -323,13 +237,47 @@ namespace DenTech
             }
         }
 
-        // Método ModificarCita
+        // Evento ModificarCita
         private void ModificarCita(object sender, EventArgs e)
         {
             // Se transifere la información del Id para modificar su registro en la ventana
             LinkLabel tag = (LinkLabel)sender;
-            int Id = Int32.Parse(tag.Tag.ToString());
+            this.Id = Int32.Parse(tag.Tag.ToString());
+            Menu_ModEli.Show(tag, new Point(0, tag.Height));
+        }
+
+        // Evento de la opción Modificar del Popup
+        private void MenuOp_Modificar_Click(object sender, EventArgs e)
+        {
+            // Abre la ventana
             WIN_CAT_Citas_F Window = new WIN_CAT_Citas_F(Id);
+            Window.ShowDialog();
+            MostrarFechaActual();
+        }
+
+        // Evento de la opción Eliminar del Popup
+        private void MenuOp_Eliminar_Click(object sender, EventArgs e)
+        {
+            // Elimina
+            // Pregunta al usuario si desea eliminar el registro
+            if (MessageBox.Show("¿Desea eliminar el registro seleccionado?", "DenTech", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                // Se estructura el query para eliminar el registro
+                SqlCommand cmd = BD.conexion.CreateCommand();
+                cmd.CommandText = "Delete From CITAS Where Id_Cita = " + Id;
+                cmd.ExecuteNonQuery(); // Se ejecuta
+
+                // Se confirma la eliminación del registro y se actualiza la información de la tabla
+                MessageBox.Show("Registro eliminado con éxito.", "DenTech", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MostrarFechaActual();
+            }
+        }
+
+        // Evento de la opción Recetas del Popup
+        private void recetasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Se insatncia un objeto de tipo ventana para abrirla y refrescar la tabla
+            WIN_CAT_Recetas_T Window = new WIN_CAT_Recetas_T(this.Id);
             Window.ShowDialog();
             MostrarFechaActual();
         }
