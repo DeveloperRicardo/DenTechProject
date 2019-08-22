04d48e70658fbf79b631daa1e5299e2459ee5c56
Sebastian Valera <sebastianvalera99@outlook.com>
2019-08-22 00:06:58 -0700

    mas correcciones

--- DenTech/WIN_CAT_Receta-Repo.Designer.cs
+++ DenTech/WIN_CAT_Receta-Repo.Designer.cs
@@ -28,19 +28,294 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WIN_CAT_Receta_Repo));
+            this.button1 = new System.Windows.Forms.Button();
+            this.BTN_Aceptar = new System.Windows.Forms.Button();
+            this.EDT_Diagnostico = new System.Windows.Forms.TextBox();
+            this.STC_Diagnostico = new System.Windows.Forms.Label();
+            this.EDT_Medicamento = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EDT_Tratamiento = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EDT_FechaInicio = new System.Windows.Forms.DateTimePicker();
+            this.STC_Fecha = new System.Windows.Forms.Label();
+            this.EDT_FechaFinal = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EDT_FechaDiag = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PANEL_Odontologo = new System.Windows.Forms.Panel();
+            this.STC_NombreOdontologo = new System.Windows.Forms.Label();
+            this.BTN_Odontologo = new System.Windows.Forms.Button();
+            this.STC_Odontologo = new System.Windows.Forms.Label();
+            this.PANEL_Odontologo.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(609, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 52);
+            this.button1.TabIndex = 9;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BTN_Aceptar
+            // 
+            this.BTN_Aceptar.BackColor = System.Drawing.Color.Transparent;
+            this.BTN_Aceptar.FlatAppearance.BorderSize = 0;
+            this.BTN_Aceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_Aceptar.Image = ((System.Drawing.Image)(resources.GetObject("BTN_Aceptar.Image")));
+            this.BTN_Aceptar.Location = new System.Drawing.Point(551, 369);
+            this.BTN_Aceptar.Name = "BTN_Aceptar";
+            this.BTN_Aceptar.Size = new System.Drawing.Size(52, 52);
+            this.BTN_Aceptar.TabIndex = 8;
+            this.BTN_Aceptar.UseVisualStyleBackColor = false;
+            this.BTN_Aceptar.Click += new System.EventHandler(this.BTN_Aceptar_Click);
+            // 
+            // EDT_Diagnostico
+            // 
+            this.EDT_Diagnostico.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EDT_Diagnostico.Location = new System.Drawing.Point(126, 144);
+            this.EDT_Diagnostico.MaxLength = 30;
+            this.EDT_Diagnostico.Multiline = true;
+            this.EDT_Diagnostico.Name = "EDT_Diagnostico";
+            this.EDT_Diagnostico.Size = new System.Drawing.Size(535, 56);
+            this.EDT_Diagnostico.TabIndex = 3;
+            // 
+            // STC_Diagnostico
+            // 
+            this.STC_Diagnostico.AutoSize = true;
+            this.STC_Diagnostico.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.STC_Diagnostico.Location = new System.Drawing.Point(20, 144);
+            this.STC_Diagnostico.Name = "STC_Diagnostico";
+            this.STC_Diagnostico.Size = new System.Drawing.Size(92, 20);
+            this.STC_Diagnostico.TabIndex = 20;
+            this.STC_Diagnostico.Text = "Diagnostico:";
+            // 
+            // EDT_Medicamento
+            // 
+            this.EDT_Medicamento.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EDT_Medicamento.Location = new System.Drawing.Point(126, 206);
+            this.EDT_Medicamento.MaxLength = 30;
+            this.EDT_Medicamento.Multiline = true;
+            this.EDT_Medicamento.Name = "EDT_Medicamento";
+            this.EDT_Medicamento.Size = new System.Drawing.Size(535, 56);
+            this.EDT_Medicamento.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Medicamento:";
+            // 
+            // EDT_Tratamiento
+            // 
+            this.EDT_Tratamiento.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EDT_Tratamiento.Location = new System.Drawing.Point(126, 268);
+            this.EDT_Tratamiento.MaxLength = 30;
+            this.EDT_Tratamiento.Multiline = true;
+            this.EDT_Tratamiento.Name = "EDT_Tratamiento";
+            this.EDT_Tratamiento.Size = new System.Drawing.Size(535, 56);
+            this.EDT_Tratamiento.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Tratamiento:";
+            // 
+            // EDT_FechaInicio
+            // 
+            this.EDT_FechaInicio.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.EDT_FechaInicio.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.EDT_FechaInicio.Location = new System.Drawing.Point(126, 330);
+            this.EDT_FechaInicio.Name = "EDT_FechaInicio";
+            this.EDT_FechaInicio.Size = new System.Drawing.Size(112, 25);
+            this.EDT_FechaInicio.TabIndex = 6;
+            this.EDT_FechaInicio.Value = new System.DateTime(2019, 7, 28, 0, 0, 0, 0);
+            // 
+            // STC_Fecha
+            // 
+            this.STC_Fecha.AutoSize = true;
+            this.STC_Fecha.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.STC_Fecha.Location = new System.Drawing.Point(20, 333);
+            this.STC_Fecha.Name = "STC_Fecha";
+            this.STC_Fecha.Size = new System.Drawing.Size(90, 20);
+            this.STC_Fecha.TabIndex = 50;
+            this.STC_Fecha.Text = "Fecha Inicio:";
+            // 
+            // EDT_FechaFinal
+            // 
+            this.EDT_FechaFinal.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.EDT_FechaFinal.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.EDT_FechaFinal.Location = new System.Drawing.Point(383, 329);
+            this.EDT_FechaFinal.Name = "EDT_FechaFinal";
+            this.EDT_FechaFinal.Size = new System.Drawing.Size(112, 25);
+            this.EDT_FechaFinal.TabIndex = 7;
+            this.EDT_FechaFinal.Value = new System.DateTime(2019, 7, 28, 0, 0, 0, 0);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(277, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 52;
+            this.label3.Text = "Fecha Final:";
+            // 
+            // EDT_FechaDiag
+            // 
+            this.EDT_FechaDiag.Enabled = false;
+            this.EDT_FechaDiag.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.EDT_FechaDiag.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.EDT_FechaDiag.Location = new System.Drawing.Point(126, 82);
+            this.EDT_FechaDiag.Name = "EDT_FechaDiag";
+            this.EDT_FechaDiag.Size = new System.Drawing.Size(112, 25);
+            this.EDT_FechaDiag.TabIndex = 1;
+            this.EDT_FechaDiag.Value = new System.DateTime(2019, 7, 28, 0, 0, 0, 0);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Enabled = false;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 20);
+            this.label4.TabIndex = 54;
+            this.label4.Text = "Fecha Diag.:";
+            // 
+            // PANEL_Odontologo
+            // 
+            this.PANEL_Odontologo.BackColor = System.Drawing.Color.White;
+            this.PANEL_Odontologo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PANEL_Odontologo.Controls.Add(this.STC_NombreOdontologo);
+            this.PANEL_Odontologo.Location = new System.Drawing.Point(126, 113);
+            this.PANEL_Odontologo.Name = "PANEL_Odontologo";
+            this.PANEL_Odontologo.Size = new System.Drawing.Size(313, 25);
+            this.PANEL_Odontologo.TabIndex = 2;
+            // 
+            // STC_NombreOdontologo
+            // 
+            this.STC_NombreOdontologo.AutoSize = true;
+            this.STC_NombreOdontologo.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.STC_NombreOdontologo.ForeColor = System.Drawing.Color.Black;
+            this.STC_NombreOdontologo.Location = new System.Drawing.Point(3, 1);
+            this.STC_NombreOdontologo.Name = "STC_NombreOdontologo";
+            this.STC_NombreOdontologo.Size = new System.Drawing.Size(47, 20);
+            this.STC_NombreOdontologo.TabIndex = 4;
+            this.STC_NombreOdontologo.Text = "Título";
+            // 
+            // BTN_Odontologo
+            // 
+            this.BTN_Odontologo.BackColor = System.Drawing.Color.White;
+            this.BTN_Odontologo.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BTN_Odontologo.FlatAppearance.BorderSize = 2;
+            this.BTN_Odontologo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_Odontologo.Font = new System.Drawing.Font("Rockwell", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_Odontologo.Image = ((System.Drawing.Image)(resources.GetObject("BTN_Odontologo.Image")));
+            this.BTN_Odontologo.Location = new System.Drawing.Point(445, 113);
+            this.BTN_Odontologo.Name = "BTN_Odontologo";
+            this.BTN_Odontologo.Size = new System.Drawing.Size(29, 25);
+            this.BTN_Odontologo.TabIndex = 2;
+            this.BTN_Odontologo.UseVisualStyleBackColor = false;
+            this.BTN_Odontologo.Click += new System.EventHandler(this.BTN_Odontologo_Click);
+            // 
+            // STC_Odontologo
+            // 
+            this.STC_Odontologo.AutoSize = true;
+            this.STC_Odontologo.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.STC_Odontologo.Location = new System.Drawing.Point(20, 115);
+            this.STC_Odontologo.Name = "STC_Odontologo";
+            this.STC_Odontologo.Size = new System.Drawing.Size(94, 20);
+            this.STC_Odontologo.TabIndex = 56;
+            this.STC_Odontologo.Text = "Odontólogo:";
             // 
             // WIN_CAT_Receta_Repo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Cerrar = true;
+            this.ClientSize = new System.Drawing.Size(683, 437);
+            this.Controls.Add(this.PANEL_Odontologo);
+            this.Controls.Add(this.BTN_Odontologo);
+            this.Controls.Add(this.STC_Odontologo);
+            this.Controls.Add(this.EDT_FechaDiag);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EDT_FechaFinal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EDT_FechaInicio);
+            this.Controls.Add(this.STC_Fecha);
+            this.Controls.Add(this.EDT_Tratamiento);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EDT_Medicamento);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BTN_Aceptar);
+            this.Controls.Add(this.EDT_Diagnostico);
+            this.Controls.Add(this.STC_Diagnostico);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "WIN_CAT_Receta_Repo";
-            this.Text = "WIN_CAT_Receta_Repo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TituloBanner = "Recetas";
+            this.TituloVentana = "      DenTech - Recetas";
+            this.Load += new System.EventHandler(this.WIN_CAT_Receta_Repo_Load);
+            this.Controls.SetChildIndex(this.STC_Diagnostico, 0);
+            this.Controls.SetChildIndex(this.EDT_Diagnostico, 0);
+            this.Controls.SetChildIndex(this.BTN_Aceptar, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.EDT_Medicamento, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.EDT_Tratamiento, 0);
+            this.Controls.SetChildIndex(this.STC_Fecha, 0);
+            this.Controls.SetChildIndex(this.EDT_FechaInicio, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.EDT_FechaFinal, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.EDT_FechaDiag, 0);
+            this.Controls.SetChildIndex(this.STC_Odontologo, 0);
+            this.Controls.SetChildIndex(this.BTN_Odontologo, 0);
+            this.Controls.SetChildIndex(this.PANEL_Odontologo, 0);
+            this.PANEL_Odontologo.ResumeLayout(false);
+            this.PANEL_Odontologo.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BTN_Aceptar;
+        private System.Windows.Forms.TextBox EDT_Diagnostico;
+        private System.Windows.Forms.Label STC_Diagnostico;
+        private System.Windows.Forms.TextBox EDT_Medicamento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EDT_Tratamiento;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker EDT_FechaInicio;
+        private System.Windows.Forms.Label STC_Fecha;
+        private System.Windows.Forms.DateTimePicker EDT_FechaFinal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker EDT_FechaDiag;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel PANEL_Odontologo;
+        private System.Windows.Forms.Label STC_NombreOdontologo;
+        private System.Windows.Forms.Button BTN_Odontologo;
+        private System.Windows.Forms.Label STC_Odontologo;
     }
 }
